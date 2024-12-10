64ffee716c5d01dbac6b8acbafde6f114cfc7cce
GaneshGhatti <ganeshghatti6@gmail.com>
2024-12-10 20:17:22 +0530

    initial commit

---
+++ AbhishekProjects/server/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectdatabase = async () => {
+  try {
+    // this is db from aniket mongodb account
+    await mongoose.connect(
+      "mongodb+srv://ganesh:dbb52nxJyecA1u7Q@aidcircle.uvee4.mongodb.net/test?retryWrites=true&w=majority&appName=aidcircle"
+    );
+
+    console.log("db connection successful");
+  } catch (error) {
+    console.log("db connection failed" + error.message);
+  }
+};
+module.exports = connectdatabase;
